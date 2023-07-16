d9cdaa7c99aff55a358e7a1aaf323c414e6501e4
“PRATEEK <mukeshkumarsingh.4462@gmail.com>
2023-07-16 21:17:45 +0530

    insertion sort added

--- src/DATA_STRUCTURES_FUNDAMENTALS/Search_n_Sort/Sorting_algos/Insertion_sort.java
+++ src/DATA_STRUCTURES_FUNDAMENTALS/Search_n_Sort/Sorting_algos/Insertion_sort.java
@@ -4,37 +4,30 @@ import java.util.Arrays;
 
 public class Insertion_sort {
     public static void main(String[] args) {
-        int[] arr={4,5,1,2,3,7,10,6,8,9};
-        System.out.println(Arrays.toString(insertion_sort(arr)));
+        int[] arr={5,4,3,2,1,0};
+        System.out.println(Arrays.toString(insert_sort(arr)));
     }
 
-    private static int[] insertion_sort(int[] arr) {
-        for (int i = 0; i <arr.length; i++) { //total n-1 iterations are required as first element will be sorted
-            // automatically
-             //As for n elements, n-1 comparisons are needed.
-                int lastIndex=(arr.length-1)-i; //For every iteration we should
-            // leave the already sorted elements in
-            // right extreme, and compare and swap for the remaining elements to the next last index.
-            // "Hence: lastindex= (arr.len-1)-i"=(lastindexval-i)
-                int maxIndex=findMaxIndex(arr,0,lastIndex); //finds the index with maximum element in the remaining
-            // part of unsorted array elements
-                swap(arr,maxIndex,lastIndex); //performs swapping of the max element with the "next" last element
+    static int[] insert_sort(int[] arr) {
+        for (int i = 0; i <arr.length-1; i++) { //i will perform arr.len-1 comparisons
+
+            for(int j=i+1;j>0;j--){ //j will start one place further than i initially. Its job is to take an element
+                // and comapare it with elements in sorted part of array, if it is smaller-> then take it to its
+                // appropriate position, else loop break.
+                // If it reaches index 0, loop breaks.
+                if(arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+                }else{
+                    break;
+                }
+            }
         }
         return arr;
     }
-    static void swap(int[] arr,int maxIndex,int lastIndex){
-        int temp=arr[lastIndex];
-        arr[lastIndex]=arr[maxIndex];
-        arr[maxIndex]=temp;
-    }
-
-    private static int findMaxIndex(int[] arr, int start, int end) {
-        int max=start;
-        for (int i=start;i<=end;i++){
-            if(arr[i]>arr[max]){
-                max=i;
-            }
-        }
-        return max;
+    static int[] swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+        return arr;
     }
 }
